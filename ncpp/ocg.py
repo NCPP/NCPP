6eab3b6153ec65b9b503cffed934acbf04f81709
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2013-06-28 05:13:01 -0600

    Fixing bug.

--- ncpp/ocg.py
+++ ncpp/ocg.py
@@ -121,9 +121,9 @@ class OCG(object):
             # 'path' points to the top-level folder containing the output data
             path = ops.execute()
             # 'download_path' points to single file for user to download
-            download_path = ocgis.format_return(path, ops)
+            download_path = ocgis.format_return(path, ops, with_auxiliary_files=args['with_auxiliary_files'])
 
         # return ouput
-        url = download_path.replace(self.rootDir, self.rootUrl, with_auxiliary_files=args['with_auxiliary_files'])
+        url = download_path.replace(self.rootDir, self.rootUrl)
         return url
         
