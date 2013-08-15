14c57bf17721cd3d4d6e2ba3deb3c026e84ef8cb
Ben Koziol <benkoziol@gmail.com>
2013-08-15 10:04:28 -0600

    new dataset find to pull bioclim indices

--- contrib/dataset_find.py
+++ contrib/dataset_find.py
@@ -8,7 +8,11 @@ logging.basicConfig(filename='log_df_info.log',
                     filemode='w',
                     level=logging.INFO)
 
-HEADDIRS = ['/data/downscaled','/data/staging/jvigh']
+HEADDIRS = [
+#            '/data/downscaled',
+#            '/data/staging/jvigh',
+            '/data/ncpp/eval'
+            ]
 
 
 def iter_nc():
@@ -71,23 +75,41 @@ class DataCategory(object):
 
 def get_categories():
     dcs = []
-    dcs.append(DataCategory('/data/staging/jvigh/climatology_txx.*','QED-2013 Indices','Maurer02v2 TXx',add_period=True))
-    dcs.append(DataCategory('/data/staging/jvigh/climatology_txn.*','QED-2013 Indices','Maurer02v2 TXn',add_period=True))
-    dcs.append(DataCategory('/data/staging/jvigh/climatology_tnx.*','QED-2013 Indices','Maurer02v2 TNx',add_period=True))
-    dcs.append(DataCategory('/data/staging/jvigh/climatology_tnn.*','QED-2013 Indices','Maurer02v2 TNn',add_period=True))
-    dcs.append(DataCategory('/data/staging/jvigh/climatology_fd.*','QED-2013 Indices','Maurer02v2 FD',add_period=True))
-    dcs.append(DataCategory('/data/staging/jvigh/climatology_r20mm.*','QED-2013 Indices','Maurer02v2 R20mm',add_period=True))
-    dcs.append(DataCategory('/data/staging/jvigh/climatology_tasmax.*p90.*','QED-2013 Indices','Maurer02v2 TasMax p90',add_period=True))
-    dcs.append(DataCategory('/data/staging/jvigh/climatology_tasmin.*p90.*','QED-2013 Indices','Maurer02v2 TasMin p90',add_period=True))
-    dcs.append(DataCategory('/data/staging/jvigh/climatology_pr.*p90.*','QED-2013 Indices','Maurer02v2 Pr p90',add_period=True))
-    dcs.append(DataCategory('/data/downscaled/bcca/bcca_gfdl_.*(tasmax|tasmin|pr).*','Downscaled Datasets','BCCA-GFDL'))
-    dcs.append(DataCategory('/data/downscaled/bcca/bcca_cccma_.*(tasmax|tasmin|pr).*','Downscaled Datasets','BCCA-CCCMA-CGCM'))
-    dcs.append(DataCategory('/data/downscaled/arrm/arrm_cgcm3_.*(tasmax|tasmin|pr).*','Downscaled Datasets','ARRM-CGCM (Hayhoe)'))
-    dcs.append(DataCategory('/data/downscaled/arrm/arrm_gfdl_.*(tasmax|tasmin|pr).*','Downscaled Datasets','ARRM-GFDL (Hayhoe)'))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_txx.*','QED-2013 Indices','Maurer02v2 TXx',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_txn.*','QED-2013 Indices','Maurer02v2 TXn',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_tnx.*','QED-2013 Indices','Maurer02v2 TNx',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_tnn.*','QED-2013 Indices','Maurer02v2 TNn',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_fd.*','QED-2013 Indices','Maurer02v2 FD',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_r20mm.*','QED-2013 Indices','Maurer02v2 R20mm',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_tasmax.*p90.*','QED-2013 Indices','Maurer02v2 TasMax p90',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_tasmin.*p90.*','QED-2013 Indices','Maurer02v2 TasMin p90',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_pr.*p90.*','QED-2013 Indices','Maurer02v2 Pr p90',add_period=True))
+#    dcs.append(DataCategory('/data/downscaled/bcca/bcca_gfdl_.*(tasmax|tasmin|pr).*','Downscaled Datasets','BCCA-GFDL'))
+#    dcs.append(DataCategory('/data/downscaled/bcca/bcca_cccma_.*(tasmax|tasmin|pr).*','Downscaled Datasets','BCCA-CCCMA-CGCM'))
+#    dcs.append(DataCategory('/data/downscaled/arrm/arrm_cgcm3_.*(tasmax|tasmin|pr).*','Downscaled Datasets','ARRM-CGCM (Hayhoe)'))
+#    dcs.append(DataCategory('/data/downscaled/arrm/arrm_gfdl_.*(tasmax|tasmin|pr).*','Downscaled Datasets','ARRM-GFDL (Hayhoe)'))
+    dcs.append(DataCategory('.*maurer02v2/bio.*\.nc','QED-2013 Indices','Bioclim Indices (Maurer02v2)'))
+    dcs.append(DataCategory('.*bcca_gfdl_cm2_1/bio.*\.nc','QED-2013 Indices','Bioclim Indices (BCCA-GFDL-CM-2.1)'))
+    dcs.append(DataCategory('.*bcca_cccma.*/bio.*\.nc','QED-2013 Indices','Bioclim Indices (BCCA-CCCMA-CGCM-3.1)'))
+    dcs.append(DataCategory('.*arrm_cgcm3_t47/bio.*\.nc','QED-2013 Indices','Bioclim Indices (ARMM-CGCM-3-t47)'))
+    dcs.append(DataCategory('.*arrm_cgcm3_t63/bio.*\.nc','QED-2013 Indices','Bioclim Indices (ARMM-CGCM-3-t63)'))
+    dcs.append(DataCategory('.*arrm_gfdl_2\.1/bio.*\.nc','QED-2013 Indices','Bioclim Indices (ARMM-GFDL-2.1)'))
     
     with open('df_info.csv','w') as f:
         writer = csv.DictWriter(f,['Category','Subcategory','Directory Path','Filename','Variable'])
-        writer.writeheader()
+        writer.writeheader()#    dcs.append(DataCategory('/data/staging/jvigh/climatology_txx.*','QED-2013 Indices','Maurer02v2 TXx',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_txn.*','QED-2013 Indices','Maurer02v2 TXn',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_tnx.*','QED-2013 Indices','Maurer02v2 TNx',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_tnn.*','QED-2013 Indices','Maurer02v2 TNn',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_fd.*','QED-2013 Indices','Maurer02v2 FD',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_r20mm.*','QED-2013 Indices','Maurer02v2 R20mm',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_tasmax.*p90.*','QED-2013 Indices','Maurer02v2 TasMax p90',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_tasmin.*p90.*','QED-2013 Indices','Maurer02v2 TasMin p90',add_period=True))
+#    dcs.append(DataCategory('/data/staging/jvigh/climatology_pr.*p90.*','QED-2013 Indices','Maurer02v2 Pr p90',add_period=True))
+#    dcs.append(DataCategory('/data/downscaled/bcca/bcca_gfdl_.*(tasmax|tasmin|pr).*','Downscaled Datasets','BCCA-GFDL'))
+#    dcs.append(DataCategory('/data/downscaled/bcca/bcca_cccma_.*(tasmax|tasmin|pr).*','Downscaled Datasets','BCCA-CCCMA-CGCM'))
+#    dcs.append(DataCategory('/data/downscaled/arrm/arrm_cgcm3_.*(tasmax|tasmin|pr).*','Downscaled Datasets','ARRM-CGCM (Hayhoe)'))
+#    dcs.append(DataCategory('/data/downscaled/arrm/arrm_gfdl_.*(tasmax|tasmin|pr).*','Downscaled Datasets','ARRM-GFDL (Hayhoe)'))
         for nc_path in iter_nc():
             for dc in dcs:
                 try:
