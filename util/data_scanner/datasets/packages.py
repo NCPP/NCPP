577b1fb314d07b2d3c28d103e3b5b74a2fe14bee
Ben Koziol <benkoziol@gmail.com>
2013-10-22 17:27:07 -0600

    mark - minor

--- util/data_scanner/datasets/packages.py
+++ util/data_scanner/datasets/packages.py
@@ -1,22 +1,22 @@
-from base import AbstractDataPackage
+import base
 import maurer
 import hayhoe
 
 
-class MaurerPackage(AbstractDataPackage):
+class MaurerPackage(base.AbstractDataPackage):
     name = 'Maurer 2010'
     description = '<tdk>'
     fields = [maurer.MaurerTas,
               maurer.MaurerTasmax,
               maurer.MaurerTasmin,
               maurer.MaurerPrecip]
-    dataset_category = dict(name='Gridded Observational Datasets',description='<tdk>')
+    dataset_category = base.CATEGORY_GRIDDED_OBS
     
     
-class HayhoeGFDLPackage(AbstractDataPackage):
+class HayhoeGFDLPackage(base.AbstractDataPackage):
     name = 'Hayhoe GFDL'
     description = '<tdk>'
     fields = [hayhoe.HayhoeGFDLPr,
               hayhoe.HayhoeGFDLTasmax,
               hayhoe.HayhoeGFDLTasmin]
-    dataset_category = dict(name='Gridded Observational Datasets',description='<tdk>')
+    dataset_category = dict(name='Downscaled',description='<tdk>')
